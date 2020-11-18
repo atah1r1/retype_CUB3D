ec8f07e83b3a1c32103f39769bacab47e8ce1c26
Amine Tahiri <atahiri@e1r9p12.1337.ma>
2020-11-18 18:38:20 +0100

    finishing all functions of rays (still one) && need to be refactored

--- handle_rays.c
+++ handle_rays.c
@@ -6,7 +6,7 @@
 /*   By: atahiri <atahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 11:54:06 by atahiri           #+#    #+#             */
-/*   Updated: 2020/11/18 13:21:32 by atahiri          ###   ########.fr       */
+/*   Updated: 2020/11/18 18:35:33 by atahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ void	find_horz_distance(float x_intercept, float y_intercept, int found_h_wall, 
 	wall_horz_x = 0;
 	wall_horz_y = 0;
 	while (next_horz_x >= 0 && next_horz_x <= (g_data->map->len * TILE_SIZE) &&
-			next_horz_y >= 0 && next_horz_y <= g_data->map_height * TILE_SIZE)
+			next_horz_y >= 0 && next_horz_y <= (g_data->map_height - 1) * TILE_SIZE)
 	{
 		if (wall_check(next_horz_x, g_ray[s_id].wall_up ? next_horz_y - 1 : next_horz_y))
 		{
@@ -73,5 +73,54 @@ void	find_horz_distance(float x_intercept, float y_intercept, int found_h_wall, 
 	}
 	horz_hit_dist = (found_h_wall)
 	? distance_between_points(g_player->x, g_player->y, wall_horz_x, wall_horz_y)
+	: INT_MAXX;
+	find_vert_wall_hint(xstep, ystep, x_intercept, y_intercept, s_id);
+}
+
+void	find_vert_wall_hint(float xstep, float ystep, float x_intercept, float y_intercept, int s_id)
+{
+	int		found_vert_wall;
+	float	next_vert_x;
+	float	next_vert_y;
+
+	found_vert_wall = 0;
+	x_intercept = floor(g_player->x / TILE_SIZE) * TILE_SIZE;
+	x_intercept += g_ray[s_id].wall_right ? TILE_SIZE : 0;
+	y_intercept = g_player->y + (x_intercept - g_player->x) * tan(g_ray[s_id].ray_angle);
+	xstep = TILE_SIZE;
+	xstep *= g_ray[s_id].wall_left ? -1 : 1;
+	ystep = TILE_SIZE * tan(g_ray[s_id].ray_angle);
+	ystep *= (g_ray[s_id].wall_up && ystep > 0) ? -1 : 1;
+	ystep *= (g_ray[s_id].wall_down && ystep < 0) ? -1 : 1;
+	next_vert_x = x_intercept;
+	next_vert_y = y_intercept;
+	find_vert_distance(next_vert_x, next_vert_y, found_vert_wall, xstep, ystep, s_id);
+}
+
+void	find_vert_distance(float next_vert_x, float next_vert_y, int found_vert_wall, float xstep, float ystep, int s_id)
+{
+	float	wall_vert_hit_x;
+	float	wall_vert_hit_y;
+	float	vert_hit_distance;
+
+	while (next_vert_x >= 0 && next_vert_x <= (g_data->map->len * TILE_SIZE) &&
+			next_vert_y >= 0 && next_vert_y <= (g_data->map_height - 1) * TILE_SIZE)
+	{
+		if (wall_check(g_ray[s_id].wall_left ? next_vert_x - 1 : next_vert_x, next_vert_y))
+		{
+			found_vert_wall = 1;
+			wall_vert_hit_x = next_vert_x;
+			wall_vert_hit_y = next_vert_y;
+			vert_hit_distance = distance_between_points(g_player->x, g_player->y, wall_vert_hit_x, wall_vert_hit_y);
+			break ;
+		}
+		else
+		{
+			next_vert_x += xstep;
+			next_vert_y += ystep;
+		}
+	}
+	vert_hit_distance = (found_vert_wall)
+	? distance_between_points(g_player->x, g_player->y, wall_vert_hit_x, wall_vert_hit_y)
 	: INT_MAXX;
 }
